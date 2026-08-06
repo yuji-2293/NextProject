13e3e449292fba53d232abc094c29ff81651daa8
inoueyuji <sikogaku2593@gmail.com>
2026-08-07 07:34:49 +0900

    e

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="text-red-500 font-bold text-4xl text-center py-10">
-      ******* Hello World *******
+      ******* Hello *******
       <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Click me</button>
     </div>
   );
