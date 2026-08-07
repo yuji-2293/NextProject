afc06c5755c812acc8ca8fa570c99aaa10361ee3
inoueyuji <sikogaku2593@gmail.com>
2026-08-07 12:46:52 +0900

    e

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,3 +1,7 @@
 export default function test() {
-  return <div className="text-red-500 font-bold">ddddd</div>;
+  return;
+  <>
+    <div className="text-red-500 font-bold">ddddd</div>
+    hello
+  </>;
 }
